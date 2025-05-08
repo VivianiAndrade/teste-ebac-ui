ef22cd4337acfcfc1e01cc0c3329e2de673da031
Viviani Andrade Machado <viviani.ndr@gmail.com>
2025-05-08 09:41:33 -0300

    primeiro teste automatizado

---
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Login',() => {
+    it('Deve fazer login com sucesso',() =>{
+        cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+        cy.get('#username').type('viviani.teste@teste.com.br')
+        cy.get('#password').type('teste@123')
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain' , 'Olá, viviani.teste (não é viviani.teste? Sair)')
+    })
+})
