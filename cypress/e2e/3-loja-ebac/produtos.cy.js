b1dd5a9292c7c75b40655f1257f3064a02c2a600
Viviani Andrade Machado <viviani.ndr@gmail.com>
2025-05-08 18:45:20 -0300

    page Objects

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,19 +1,31 @@
 /// <reference types="cypress"/>
 
+import produtosPage from "../../support/page-objects/produtos.page";
+
 describe('Funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitarUrl()
     });
 
     it('Deve selecionar um produto da lista', () => {
-        cy.get(' .products > .row')
-        //.first()
-        //.last()
-        //.eq(2)
-        .contains('Apollo Running Short')
-        .click()
-
+        produtosPage.buscarProdutoLista('Ariel Roll Sleeve Sweatshirt')
         cy.get('#tab-title-description > a').should('contain', 'Descrição')
     });
+
+    it.only('Deve um produto com sucesso', () => {
+        let produto = 'Eos V-Neck Hoodie'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain', produto)
+       
+    });
+
+    it('Deve visitar a página do produto', () => {
+       
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
+       
+    });
+
 });
